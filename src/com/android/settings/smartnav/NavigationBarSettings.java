ee3910f35e443af0128460fb9e26d32b62bbc159
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2018-12-03 22:53:22 -0500

    SmartNavSettings: AOSiP-ify
    Change-Id: I9cbaa54fbc468836af0504de60d5b488c68aba55
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/android/settings/smartnav/NavigationBarSettings.java
+++ src/com/android/settings/smartnav/NavigationBarSettings.java
@@ -32,7 +32,7 @@ import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 
-import com.invictrix.support.preferences.CustomSeekBarPreference;
+import com.aosip.owlsnest.preference.CustomSeekBarPreference;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
@@ -189,6 +189,6 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.INVICTRIX;
+        return MetricsProto.MetricsEvent.OWLSNEST;
     }
 }
